6610fd88d2a8ae9663ab84f2e84c7c45da445b7a
lightningkay <ew_34@sina.com>
2017-06-21 01:01:21 -0400

    fix

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if  [ ! -d ~/.vim/bundle/Vundle.vim ]; then
-    echo "Vundle exist"
+    echo "Vundle no exist"
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 cp ./.vimrc ~/
